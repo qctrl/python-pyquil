e42dc3976a91a8c5c784dabf3da5b8922cab10c6
Junkai Zeng <junkaizeng@Junkais-MacBook-Pro.local>
2019-11-13 16:31:05 -0800

    added tests and __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,9 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-__version__ = "0.0.1"
-
-from .program import convert_dds_to_pyquil_program
-
-__all__ = ['convert_dds_to_pyquil_program']
