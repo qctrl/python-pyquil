1d25993173198f4ab82ad40e46c5e8b7d72895d6
Leonardo Andreta de Castro <leoadec@gmail.com>
2022-12-08 17:35:23 +1100

    chore: Add deprecation warning

--- qctrlpyquil/__init__.py
+++ qctrlpyquil/__init__.py
@@ -20,6 +20,14 @@ qctrlpyquil
 
 __version__ = "0.0.8"
 
+from warnings import warn
+
 from .program import convert_dds_to_pyquil_program
 
 __all__ = ["convert_dds_to_pyquil_program"]
+
+warn(
+    "⚠️ (DEPRECATED) This package has been deprecated ⚠️\n"
+    "For updated information about how to integrate Q-CTRL software with PyQuil, "
+    "visit https://docs.q-ctrl.com"
+)
